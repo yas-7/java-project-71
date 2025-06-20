f026d3c773bf8835ed62cb3d5bd66b89fc2aff81
yas-7 <kasmenai@gmail.com>
2025-06-20 17:15:02 +0300

    step3: help info

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.13.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.7")
 }
-
 tasks.test {
     useJUnitPlatform()
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,13 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
