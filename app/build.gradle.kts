6b1dac58f00ac112d4d630281976273617633013
yas-7 <kasmenai@gmail.com>
2025-06-24 10:21:24 +0300

    step5: read & parse file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.13.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("info.picocli:picocli:4.7.7")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
 }
 tasks.test {
     useJUnitPlatform()
